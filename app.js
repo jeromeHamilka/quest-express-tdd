af58590be7ec93b100162a653b008bf77258d5df
Jérôme HAMILKA <jerome.hamilka@gmail.com>
2020-06-08 23:37:15 +0200

    first test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 module.exports = app;
